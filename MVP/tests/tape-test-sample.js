1fa1c5467708b56cbb7525cc57de9f34d5fd51f1
John Ivan <johnbauzon@gmail.com>
2017-06-19 15:30:54 -0700

    merge

--- MVP/tests/tape-test-sample.js
+++ MVP/tests/tape-test-sample.js
@@ -1,7 +1,7 @@
-const test = require( 'tape' ); 
+/* dabconst test = require( 'tape' ); 
 const operations = require('/Users/john-ivan/Documents/Codesmith/Production-Project-dAB.js/dab.js/MVP/src/operations.js');
 
-test('Negative or Odd', function (t) {
+/* dabtest('Negative or Odd', function (t) {
   let  arr = [1,2,3,4];
   let 
   arr = {hello: 'world'};
